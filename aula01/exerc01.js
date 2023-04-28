afc599ecb907c1e0d739749a1df32d886fec2280
SamuelBelloSB <samuelbellocontato@gmail.com>
2023-04-28 16:38:34 -0300

    implementando javascript no arquivo index.html

---
+++ aula01/exerc01.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é Samuel. Estou apredendo java script às ', 16,'h da tarde.')
---
+++ aula01/index.js
@@ -0,0 +1,2 @@
+console.log('Hello Mundo!');
+console.log('Meu nome é Samuel. Estou apredendo java script às ', 16,'h da tarde.');
