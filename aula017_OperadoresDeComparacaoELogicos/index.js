274a557246ed6ff68cc51f9dc7cae9c6d3538c27
Samuel Bello <samuelbellocontato@gmail.com>
2024-01-19 21:21:17 -0300

    Operadores lógicos e de comparação, estruturas condicionais, modelo base htmlcss para exercicio

---
+++ aula017_OperadoresDeComparacaoELogicos/index.js
@@ -0,0 +1,18 @@
+/**
+ * Operadores de Comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor ou igual a
+ * == igualdade (valor) **** não recomendado
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor) **** não recomendado
+ * !== diferente estrito (valor e tipo)
+ */
+
+/**
+ * Operadores Lógicos
+ * && -> AND 
+ * || -> OR
+ * !  -> NOT
+ */
