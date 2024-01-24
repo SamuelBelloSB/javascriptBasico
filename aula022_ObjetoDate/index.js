91b6564dbd09d951482158c058b146d872e124ff
Samuel Bello <samuelbellocontato@gmail.com>
2024-01-24 19:16:28 -0300

    Objeto date e switch case

---
+++ aula022_ObjetoDate/index.js
@@ -0,0 +1,21 @@
+// Função construtora Date
+// ano, mes, dia, hora, minuto, ssegundo, milissegundo
+
+function zeroAEsquerda(num) {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
+
+function formataData(data) {
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());
+  const min = zeroAEsquerda(data.getMinutes());
+  const seg = zeroAEsquerda(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
