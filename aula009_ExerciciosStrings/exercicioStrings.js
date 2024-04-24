5333cb82254bc55e502f7329108d974fbf3a6b96
Smauel Bello <samuelbellocontato@gmail.com>
2024-04-24 15:56:41 -0300

    Exercicios strings revisão

--- aula009_ExerciciosStrings/exercicioStrings.js
+++ aula009_ExerciciosStrings/exercicioStrings.js
@@ -4,12 +4,12 @@ let StringTest2 = 'testar comparação'
 
 // console.log(StringTest.charAt(2));
 
-console.log(StringTest.)
+console.log(StringTest)
 let num1 = 1;
-let num2 = 20;
+let num2 = 200;
 let num3 = 14.53626674450993;
 //  Concatenação de number, basta usar .toString() para alterar o valor da variável para uma string.
-console.log('Concatenação de number', num1.toString() + num2);
+console.log('Concatenação de number', (num1 + num2).toString());
 //  Conversão do number para valor binário:
 console.log(`Conversão do number ${num2} para valor binário: ${num2.toString(2)}`);
 //  Definir número de casas decimais:
